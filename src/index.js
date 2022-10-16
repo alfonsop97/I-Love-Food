21b513080b8c9454e20fdfcf9bce2d480a472b8a
Alfonso Pimentel <alfonsopimentel@Alfonsos-MacBook-Pro.local>
2022-10-15 17:43:42 -0700

    added auth

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from "aws-amplify";
-import awsconfig from "./aws-exports";
+import config from './aws-exports';
 
-Amplify.configure(awsconfig);
+Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
